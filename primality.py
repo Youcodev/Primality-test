f7b7c99c4de0e4c610324283885d178da5c50464
Youcodev <emailforgitupdates@gmail.com>
2018-12-30 19:57:16 -0500

    Return in the output message the lowest factor of a composite input number

--- primality.py
+++ primality.py
@@ -5,19 +5,25 @@
 # given that all prime numbers, except 2 and 3, are of this form
 
 def primality(n):
-    if(n == 2 or n == 3):
-        return True
-    if(n % 2 == 0 or n % 3 == 0):
+    if(n == 2 or n == 3): # 2 and 3 are prime numbers
+        return True 
+    if(n % 2 == 0): # Input number is even
+        primality.m = 2
         return False
-    m = 5
-    while(m * m <= n):
+    if(n % 3 == 0): # Input number is divisible by 3
+        primality.m = 3
+        return False
+    m = 5 # Trying numbers of the form 6k ± 1 up to the square root of the input number
+    while(m * m <= n): 
         if(n % m == 0):
+            primality.m = m
             return False
         m = m + 2
         if(n % m == 0):
+            primality.m = m
             return False
         m = m + 4
     return True
 
 nbr = input("\nHello ! Enter a number to check if it's prime: ")
-print("\nThe number you entered is prime") if primality(int(nbr)) else print("\nThe number you entered is not prime")
+print("\nThe number you entered is prime") if primality(int(nbr)) else print("\nThe number you entered is not prime. It's divisible by " + str(primality.m)) 
